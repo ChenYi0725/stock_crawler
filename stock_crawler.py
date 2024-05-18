62ee109f7229f0bc5bf97b6296a8db138d9013c9
eason <eason.jshouse0725@gmail.com>
2024-05-19 01:46:49 +0800

    turn multidata into dataframe

--- stock_crawler.py
+++ stock_crawler.py
@@ -9,18 +9,30 @@ chrome_option = webdriver.ChromeOptions()
 chrome_option.add_experimental_option("detach", value=True)
 driver = webdriver.Chrome(options=chrome_option)
 
-driver.get("https://tw.stock.yahoo.com/quote/1101.TW")
 
-
-table = driver.find_element(
-    By.XPATH, '//*[@id="qsp-overview-realtime-info"]/div[2]/div[2]/div/ul'
-)
-li_elements = table.find_elements(By.XPATH, "./li")
-
-print(li_elements)
+codes = ["1101.TW", "2330.TW", "2317.TW"]
 data = []
-for element in li_elements:
-    data.append(element.text)
-for item in data:
-    print(item)
+titles = []
+is_titles_acquired = False
+for code in codes:
+    driver.get(f"https://tw.stock.yahoo.com/quote/{code}")
+    table = driver.find_element(
+        By.XPATH, '//*[@id="qsp-overview-realtime-info"]/div[2]/div[2]/div/ul'
+    )
+    li_elements = table.find_elements(By.XPATH, "./li")
+    data_row = []
+    for li in li_elements:
+        spans = li.find_elements(By.XPATH, "./span")
+        data_row.append(spans[1].text)
+        if not is_titles_acquired:
+            titles.append(spans[0].text)
+    data.append(data_row)
+    is_titles_acquired = True
 driver.quit()
+df = pd.DataFrame(data, columns=titles)
+
+print(df)
+
+
+# titles = data[::2]
+# contents = data[1::2]
